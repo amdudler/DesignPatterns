ec0e9670637ec3c1ed9200f577130a0c357e51d3
amdudler <amir.duratovic@student.htldornbirn.at>
2026-06-03 11:22:51 +0200

    10 Games Factory finished, starting with Singleton

--- MyGame.ts
+++ MyGame.ts
@@ -9,31 +9,18 @@ import { RightMovement } from "./movements/RightMovement.js";
 import { LeftMovement } from "./movements/LeftMovement.js";
 import { Homer } from "./actors/Homer.js";
 import { Observer } from "./observer/Observer.js";
+import { AbstractActor } from "./actors/AbstractActor.js";
+import { ActorFactory } from "./ActorFactory.js";
 
 class MyGame extends Game {
   private actors: Actor[] = [];
   private observers: Observer[] = [];
 
   init(): void {
-    console.log("Game started!");
-    const r1 = new Rectangle(new RightMovement(50, 50, 100), 50, 50);
-    const r2 = new Rectangle(new LeftMovement(100, 100, 100), 20, 20);
-    const r3 = new Rectangle(new RightMovement(200, 150, 300), 50, 50
-    );
-    const c1: Circle = new Circle(new LeftMovement(300, 200, 150), 30);
-    const c2: Circle = new Circle(new RightMovement(400, 250, 200), 40
-    );
-    this.actors.push(r1, r2, r3, c1, c2);
-    const t1 = new Tree(500, 500, 150);
-    this.actors.push(t1);
-    this.actors.push(new Tree(50, 250, 100));
-    this.actors.push(new Tree(150, 500, 100));
-    this.actors.push(new Homer(new RightMovement(400, 400, 200)));
-
-    this.addObserver(c1);
-    this.addObserver(c2);
-    this.addObserver(r1);
-    this.addObserver(t1);
+    const acts: Actor[] = [];
+    const factory = new ActorFactory(800, 600);
+    acts.push(...factory.createRandomActors(10));
+    this.actors.push(...acts);
   }
 
   update(deltaTime: number): void {
